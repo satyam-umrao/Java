8de281172034288fabd2fdafb15fb5eadf344aaf
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-26 16:55:51 +0530

    small changes

--- star_patterns/pattern7.java
+++ star_patterns/pattern7.java
@@ -2,7 +2,7 @@ package star_patterns;
 
 public class pattern7 {
     public static void main(String[] args) {
-        // simple butterfly pattern
+        // butterfly pattern
         int n = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
