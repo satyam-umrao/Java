61cb05e68eee21c7b491a9a587b754fe382c7b9c
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-26 16:34:52 +0530

    thollow triangle

--- star_patterns/pattern6.java
+++ star_patterns/pattern6.java
@@ -1,5 +1,22 @@
 package star_patterns;
 
 public class pattern6 {
-    
+    public static void main(String[] args) {
+        // simple triangle pattern
+        int n=10;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i ; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(i*2)-1 ; k++){
+                if(k==1 || k==(i*2)-1 || i==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
 }
