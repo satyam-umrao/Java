7a07d5f435d0be0c2d9fcd7eb3c9dcbefa0bcedb
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-26 16:51:49 +0530

    Butteryfly pattern

--- star_patterns/pattern7.java
+++ star_patterns/pattern7.java
@@ -1,5 +1,32 @@
 package star_patterns;
 
 public class pattern7 {
-    
+    public static void main(String[] args) {
+        // simple butterfly pattern
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
