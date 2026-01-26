829312dc509c243412d125002d2692bf01180cec
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-26 13:48:37 +0530

    right angle triangle pattern

---
+++ star_patterns/pattern2.java
@@ -0,0 +1,13 @@
+package star_patterns;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        // simple right angle triangle star pattern
+        for(int i=1 ; i<=5 ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
