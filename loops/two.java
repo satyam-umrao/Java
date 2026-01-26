76eba83303fd5704757c9b86c764322c236ea815
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-26 12:48:15 +0530

    first commit

---
+++ loops/two.java
@@ -0,0 +1,47 @@
+package loops;
+public class two {
+    public static void main(String[] args) {
+        // <<<-------------- for loop --------------->>>
+        // int count = 1;
+        // System.out.println("sum of first 5 numbers:");
+        // for(int i=0 ; i<6 ; i++){
+        //     count = count+i;
+        //     System.out.print(i+" ");
+        // }
+        // System.out.println("\nis : "+count);
+
+        // <<<-------------- while loop --------------->>>
+        // int count = 0;
+        // int i=1;
+        // System.out.println("sum of first 5 numbers:");
+        // while(i<6){
+        //     count += i;
+        //     System.out.print(i+" ");
+        //     i++;
+        // }
+        // System.out.println("\nis : "+count);
+
+        // <<<-------------- do while loop --------------->>>
+        // int count = 0;
+        // int i=1;
+        // System.out.println("sum of first 5 numbers:");
+        // do{
+        //     count += i;
+        //     System.out.print(i+" ");
+        //     i++;
+        // }while(i<6);
+        // System.out.println("\nis : "+count);
+    }
+}
+
+
+/* <<<------------------------ README ------------------------>>> */
+// Continue : Used to skip the current iteration.
+// Break    : Used to terminate the loop entirely.
+
+/* <<<--------------------- Example ---------------------->>> */
+// for(int i=1 ; i<=10 ; i++) continue;
+// System.out.println("Continued");
+
+// for(int i=1 ; i<=10 ; i++) break;
+// System.out.println("Broken");
---
+++ switch/one.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class one {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1 number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter 2 number : ");
+        int b = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter your Choice : ");
+        System.out.print("add, mul, sub, div\nchoice : ");
+        String c = sc.nextLine();
+        switch (c) {
+            case "add" :System.out.println(a + b);
+                break;
+            case "sub" :System.out.println(a - b);
+                break;
+            case "mul" :System.out.println(a * b);
+                break;
+            case "div" :System.out.println(a / b);
+            default    :System.out.println("Ibnvalid Choice");
+        }
+
+        sc.close();
+    }
+}
