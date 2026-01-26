9e968f1ed4c2da33af45ea412ff7eb2460a7b51c
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-26 14:04:48 +0530

    daimond pattern

---
+++ star_patterns/pattern4.java
@@ -0,0 +1,26 @@
+package star_patterns;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        // Daimond pattern
+        for(int i=1; i<=10 ; i++){
+            for(int j=10; j>=i ; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i ; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+        for(int i=9; i>=1 ; i--){
+            for(int j=10; j>=i ; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i ; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
