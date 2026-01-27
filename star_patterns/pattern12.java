ef0d5b192387022c317e3417054b37334347d80e
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-27 20:02:22 +0530

    pattern13.java

--- star_patterns/pattern12.java
+++ star_patterns/pattern12.java
@@ -2,6 +2,15 @@ package star_patterns;
 
 public class pattern12 {
     public static void main(String[] args) {
-        
+        int n=4;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n-i ; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n ; j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
     }
 }
