00877c7a8e8f4600fb6983f2ae0ede81a0fa581c
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-27 21:40:11 +0530

    person is eligible to vote or not

---
+++ functions/AgeVote.java
@@ -0,0 +1,24 @@
+package functions;
+
+import java.util.Scanner;
+
+public class AgeVote {
+    public static String isEligibleToVote(int age) {
+        if (age >= 18 && age <= 60) {
+            if(age >= 60){
+                return ("You are a senior citizen but eligible to vote.");
+            }
+            return ("You are eligible to vote.");
+        } else {
+            return ("You are not eligible to vote.");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        String result = isEligibleToVote(age);
+        System.out.println(result);
+        sc.close();
+    }
+}
