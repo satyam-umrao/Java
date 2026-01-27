0e1b5f8fc466aa5e9dc7cb096987631e00548f2f
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-27 19:33:05 +0530

    pattern 1 code added

--- star_patterns/pattern10.java
+++ star_patterns/pattern10.java
@@ -1,5 +1,17 @@
 package star_patterns;
 
 public class pattern10 {
-
+    public static void main(String[] args) {
+        
+        int n = 5;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n-i ; j++){
+                System.out.print(" ");
+            }
+            for(int j=1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
