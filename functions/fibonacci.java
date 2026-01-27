38575ef33f9a0e566ae499fb0e1cbf2a27f6b4d7
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-27 22:09:45 +0530

    fibonacci series

--- functions/fibonacci.java
+++ functions/fibonacci.java
@@ -1,5 +1,32 @@
 package functions;
 
+import java.util.*;
+
 public class fibonacci {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to find Fibonacci series up to that number: ");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c;
+        System.out.println("Fibonacci series up to " + n + ":");
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        for (int i = 0; i < n-2; i++) {
+            c=a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+        sc.close();
+    }
+
+//  <<<------------------------------------Output------------------------------------>>>
+
+// Enter a number to find Fibonacci series up to that number: 10
+// Fibonacci series up to 10:
+// 0 1 1 2 3 5 8 13 21 34 
+ 
+
 }
