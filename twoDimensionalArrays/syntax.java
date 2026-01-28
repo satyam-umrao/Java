ff3e3cfd3f4c5cf34b3ffc4d1bc0b067991b9e5a
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-28 22:59:10 +0530

    code added

--- twoDimensionalArrays/syntax.java
+++ twoDimensionalArrays/syntax.java
@@ -1,5 +1,38 @@
 package twoDimensionalArrays;
 
+import java.util.Scanner;
+
 public class syntax {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // input no of rows and columns
+        System.out.print("Enter no of rows : ");
+        int rows = sc.nextInt();
+        System.out.print("Enter no of columns : ");
+        int columns = sc.nextInt();
+
+        // matrix initialize
+        int matrix[][] = new int[rows][columns];
+
+        // Input matrix elements
+        System.out.println("Enter matrix Elements : ");
+        for(int i=0 ; i<rows ; i++){
+            for(int j=0 ; j<columns ; j++){
+                 matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // output matrix elements
+        System.out.println("Matrix elements are : ");
+        for(int i=0 ; i<rows ; i++){
+            for(int j=0 ; j<columns ; j++){
+                 System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println("");
+        } 
+        sc.close();      
+    }
 }
+
+
