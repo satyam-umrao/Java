b86e2b91f81dc7d4fedd31ca6eb9a600c35c6f04
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-28 22:24:00 +0530

    code added

--- arrays/findNumberInArray.java
+++ arrays/findNumberInArray.java
@@ -1,5 +1,30 @@
 package arrays;
-
+import java.util.Scanner;
 public class findNumberInArray {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size : ");
+        int size = sc.nextInt();
+
+        // array initialize
+        int arr[] = new int[size];
+
+        // input
+        System.out.print("Enter array elements : ");
+        for(int i=0 ; i<size ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // number to find
+        System.out.print("Enter number to find : ");
+        int num = sc.nextInt();
+
+        // find number in array
+        for(int i=0 ; i<size ; i++){
+            if(arr[i] == num){
+                System.out.print("Number found at index %d".formatted(i));
+            }
+        }
+        sc.close();
+    }
 }
