a6c3f8eb04b944f68bc2e66061c2f56b34b201d8
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-29 22:40:15 +0530

    output added

--- Java_strings/syntax.java
+++ Java_strings/syntax.java
@@ -1,5 +1,11 @@
 package Java_strings;
 
 public class syntax {
-    
+    public static void main(String[] args) {
+        String name = "Satyam Umrao";
+        System.out.println(name);
+    }
 }
+
+// <<<----------------------------Output---------------------------->>>
+// Satyam Umrao
