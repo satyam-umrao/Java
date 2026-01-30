0ed01e178aa0d419b9341f0166cc9346ff45b6af
Satyam Umrao <satyamumrao11225@gmail.com>
2026-01-30 20:26:23 +0530

    a

--- java_stringbuilder/syntax.java
+++ java_stringbuilder/syntax.java
@@ -2,6 +2,7 @@ package java_stringbuilder;
 
 public class syntax {
     public static void main(String[] args) {
-        
+       StringBuilder name = new StringBuilder("Satyam");
+       System.out.println(name);
     }
 }
