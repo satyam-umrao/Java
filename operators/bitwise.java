991d37b19226b7866034c1a76114690bc3356a36
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-02 23:55:09 +0530

    edited

--- operators/bitwise.java
+++ operators/bitwise.java
@@ -1,11 +1,11 @@
 // Bitwise operators in java
 //-------------------------------------------------------------------------------
-// & - AND           | true only if both bits are 1                             |
-// | - OR            | true if at least one bit is 1                            |
-// ^ - XOR           | true if bits are different                               | 
-// ~ - NOT           | inverts all bits                                         |
-// << - Left Shift   | shifts bits to the left                                  |
-// >> - Right Shift  | shifts bits to the right                                 |
+// & - AND                    | true only if both bits are 1                    |
+// | - OR                     | true if at least one bit is 1                   |
+// ^ - XOR                    | true if bits are different                      | 
+// ~ - NOT                    | inverts all bits                                |
+// << - Left Shift            | shifts bits to the left                         |
+// >> - Right Shift           | shifts bits to the right                        |
 // >>> - Unsigned Right Shift | shifts bits to the right, filling with zeros    |
 // ------------------------------------------------------------------------------
 
