dc2cd4122c8b5c1719dfa7aaaec22b57c459d408
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-02 23:53:41 +0530

    output added

--- operators/bitwise.java
+++ operators/bitwise.java
@@ -12,5 +12,39 @@
 package operators;
 
 public class bitwise {
+    public static void main(String[] args) {
+        int a = 5; // Binary: 0101
+        int b = 3; // Binary: 0011
 
+        // Bitwise AND
+        System.out.println("a & b = " + (a & b)); // Output: 1 (Binary: 0001)
+
+        // Bitwise OR
+        System.out.println("a | b = " + (a | b)); // Output: 7 (Binary: 0111)
+
+        // Bitwise XOR
+        System.out.println("a ^ b = " + (a ^ b)); // Output: 6 (Binary: 0110)
+
+        // Bitwise NOT
+        System.out.println("~a = " + (~a)); // Output: -6 (Binary: ...11111010)
+
+        // Left Shift
+        System.out.println("a << 1 = " + (a << 1)); // Output: 10 (Binary: 1010)
+
+        // Right Shift
+        System.out.println("a >> 1 = " + (a >> 1)); // Output: 2 (Binary: 0010)
+
+        // Unsigned Right Shift
+        System.out.println("a >>> 1 = " + (a >>> 1)); // Output: 2 (Binary: 0010)
+    }
 }
+
+// <<<-----------------------------------Output----------------------------------->>>
+//
+// a & b = 1
+// a | b = 7
+// a ^ b = 6
+// ~a = -6
+// a << 1 = 10
+// a >> 1 = 2
+// a >>> 1 = 2
