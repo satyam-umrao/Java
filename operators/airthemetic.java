79d35c07f415e331027ae586851bf88d6d63c53d
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-02 22:38:47 +0530

    theory added

--- operators/airthemetic.java
+++ operators/airthemetic.java
@@ -1,5 +1,21 @@
+// Airthemetic operators in Java
+// 1. Addition (+)
+// 2. Subtraction (-)
+// 3. Multiplication (*)
+// 4. Division (/)
+// 5. Modulus (%)
+
+// unary operators
+// 1. Increment (++)  {++x (Pre-increment) or x++ (Post-increment)}
+// 2. Decrement (--)  {--x (Pre-decrement) or x-- (Post-decrement)}
+
 package operators;
 
 public class airthemetic {
 
+    public static void main(String[] args) {
+
+    }
 }
+
+// <<<------------------------Output------------------------>>>
