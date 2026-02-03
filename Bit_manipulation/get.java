430c1ba3652c97ad800e658eb96ab8a739eed297
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-03 23:00:11 +0530

    code added

--- Bit_manipulation/get.java
+++ Bit_manipulation/get.java
@@ -2,10 +2,20 @@
 // And Operation
 
 // Example: Get the 3rd bit of number 5 (0101 in binary)
+// 1<<i
 package Bit_manipulation;
 
 public class get {
     public static void main(String[] args) {
+        int a = 5;
+        int bit = 3;
+        int left_shifted = 1 << bit;
 
+        int result = left_shifted & a;
+        if (result == 0) {
+            System.out.println("Bit is Zero");
+        } else {
+            System.out.println("Bit is One");
+        }
     }
 }
