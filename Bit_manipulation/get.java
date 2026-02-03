6982ebf5d4ebd0fdcafea87fedd9698d33970990
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-03 22:55:11 +0530

    question added

--- Bit_manipulation/get.java
+++ Bit_manipulation/get.java
@@ -1,5 +1,11 @@
+// Bit Manipulation - Get Bit
+// And Operation
+
+// Example: Get the 3rd bit of number 5 (0101 in binary)
 package Bit_manipulation;
 
 public class get {
+    public static void main(String[] args) {
 
+    }
 }
