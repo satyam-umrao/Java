0095369b80361bef6db61d72044bae0eb6b12176
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-05 22:42:30 +0530

    code added

---
+++ sorting_algos/insertion_sort.java
@@ -0,0 +1,23 @@
+package sorting_algos;
+
+public class insertion_sort {
+    public static void Sorted_array(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 7, 3, 6, 2, 1 };
+        for (int i = 0; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        Sorted_array(arr);
+    }
+}
