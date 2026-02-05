9892f076303b101c7da0d8db899e8454e48e5281
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-05 21:50:28 +0530

    code added

--- sorting_algos/Selection_sort.java
+++ sorting_algos/Selection_sort.java
@@ -1,5 +1,24 @@
 package sorting_algos;
 
 public class Selection_sort {
+    public static void Sorted_array(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 
+    public static void main(String[] args) {
+        int arr[] = { 9, 7, 3, 6, 2 };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        Sorted_array(arr);
+    }
 }
