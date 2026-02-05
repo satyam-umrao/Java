92d7ab9cc481e47863fc710bbac2dccf34e1a6e0
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-05 21:29:18 +0530

    CODE ADDED

--- sorting_algos/Bubble_sort.java
+++ sorting_algos/Bubble_sort.java
@@ -1,5 +1,24 @@
 package sorting_algos;
 
 public class Bubble_sort {
+    public static void Sorted_array(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, 7, 4, 2 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        Sorted_array(arr);
+    }
 
 }
