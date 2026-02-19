d60897cac7fa882bd6c864a58961f1b5e705a365
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-19 21:28:31 +0530

    output added

--- 13.Recursion/sumOfNums.java
+++ 13.Recursion/sumOfNums.java
@@ -4,7 +4,7 @@ public class sumOfNums {
 
     public static void sumOfNaturalNumbers(int n, int count) {
         if (n == 0) {
-            System.out.println(count);
+            System.out.println("Sum is: %d".formatted(count));
             return;
         }
         count = count + n;
@@ -13,9 +13,15 @@ public class sumOfNums {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
         int n = sc.nextInt();
         int count = 0;
         sumOfNaturalNumbers(n, count);
         sc.close();
     }
 }
+
+// Output
+//
+// Enter a number: 5
+// Sum is: 15
