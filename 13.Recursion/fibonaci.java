411aa12aa9eebe7d96c57ca0b09f0ede3846f25c
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-19 22:11:17 +0530

    code added

---
+++ 13.Recursion/fibonaci.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class fibonaci {
+    public static void fib(int n, int start, int next) {
+        int num = start + next;
+        if (num > n) {
+            return;
+        }
+        System.out.print(num + " ");
+        start = next;
+        next = num;
+        fib(n, start, next);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int n = sc.nextInt();
+
+        int start = 0;
+        int next = 1;
+        System.out.print(start + " ");
+        System.out.print(next + " ");
+        fib(n, start, next);
+        sc.close();
+    }
+}
+
+// output
+// Enter any number : 5
+// 0 1 1 2 3 5
