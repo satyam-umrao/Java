1fdd2726543909b056e4d3133fad160672ceeb89
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-19 21:37:46 +0530

    code and output added

---
+++ 13.Recursion/factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void factorial(int n, int count) {
+        if (n == 0) {
+            System.out.println("Factorial is : %d".formatted(count));
+            return;
+        }
+        count *= n;
+        factorial(n - 1, count);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int count = 1;
+        factorial(n, count);
+        sc.close();
+    }
+}
+
+// Output
+//
+// Enter a number: 5
+// Factorial is : 120
