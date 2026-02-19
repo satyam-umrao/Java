5c141ec77339263b79d133696a1495180e8d48cf
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-19 20:57:36 +0530

    recursion first code added

---
+++ 13.Recursion/reverseCounting.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class reverseCounting {
+
+    public static void reverse(int a) {
+        if (a == 0) {
+            return;
+        }
+        System.out.print(a + " ");
+        reverse(a - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int a = sc.nextInt();
+
+        reverse(a);
+        sc.close();
+    }
+}
