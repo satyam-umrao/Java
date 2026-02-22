95b48a0ad0d639ead896c5ebf4009af94b33af89
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-22 22:28:54 +0530

    code added

---
+++ 13.Recursion/powOfX_two.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class powOfX_two {
+    public static int pow(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+
+        if (n % 2 == 0) {
+            return pow(x, n / 2) * pow(x, n / 2);
+        } else {
+            return pow(x, n / 2) * pow(x, n / 2) * x;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of x: ");
+        int x = sc.nextInt();
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        int result = pow(x, n);
+        System.out.println(result);
+
+        sc.close();
+    }
+}
