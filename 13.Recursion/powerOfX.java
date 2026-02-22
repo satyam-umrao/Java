8408dc2cbd47693ee8eace6a7adf738345540529
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-22 22:07:28 +0530

    code added

--- 13.Recursion/powerOfX.java
+++ 13.Recursion/powerOfX.java
@@ -1,3 +1,28 @@
-public class powerOfX {
+import java.util.Scanner;
 
+public class powerOfX {
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        int CalcPower = power(x, n - 1);
+        int cal = (x * CalcPower);
+        return cal;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base (x): ");
+        int x = sc.nextInt();
+        System.out.print("Enter the exponent (n): ");
+        int n = sc.nextInt();
+
+        int result = power(x, n);
+        System.out.println(result);
+
+        sc.close();
+    }
 }
