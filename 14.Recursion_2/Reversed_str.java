4c6773ada683701db903dfc17523fed18a45371a
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-24 20:02:29 +0530

    code added

--- 14.Recursion_2/Reversed_str.java
+++ 14.Recursion_2/Reversed_str.java
@@ -1,3 +1,16 @@
 public class Reversed_str {
+    public static void reverse(String a, int index) {
+        if (index == 0) {
+            System.out.print(a.charAt(index));
+            return;
+        }
+        System.out.print(a.charAt(index));
+        reverse(a, index - 1);
+    }
 
+    public static void main(String[] args) {
+        String a = "Satyam";
+        int index = a.length() - 1;
+        reverse(a, index);
+    }
 }
