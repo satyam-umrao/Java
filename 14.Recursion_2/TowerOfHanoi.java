5d455bd9b42f19cefb47dcf042f1cbbfc568def7
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-24 19:19:33 +0530

    logic added

--- 14.Recursion_2/TowerOfHanoi.java
+++ 14.Recursion_2/TowerOfHanoi.java
@@ -1,12 +1,12 @@
 public class TowerOfHanoi {
     public static void Hanoi(int n, String start, String helper, String destination) {
-        if (n == 1) {
+        if (n == 1) { // if n=1 (transfer direct from start to destination)
             System.out.println("transfer of %s to %s".formatted(start, destination));
             return;
         }
-        Hanoi(n - 1, start, destination, helper);
+        Hanoi(n - 1, start, destination, helper); // first transfer big one to destination
         System.out.println("Transfer of %s to %s".formatted(start, destination));
-        Hanoi(n - 1, helper, start, destination);
+        Hanoi(n - 1, helper, start, destination); // then transfer small ones to destination through helper
     }
 
     public static void main(String[] args) {
