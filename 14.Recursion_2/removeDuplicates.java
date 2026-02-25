e3fbe706f63cdc8c465be85a6e7563e668907b13
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-25 22:42:18 +0530

    code added

---
+++ 14.Recursion_2/removeDuplicates.java
@@ -0,0 +1,24 @@
+// Remove Duplicates from array using Recursion
+public class removeDuplicates {
+    public static boolean[] map = new boolean[26];
+
+    public static void rmd(String a, int idx, String newString) {
+        if (idx == a.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char currentCharacter = a.charAt(idx);
+        if (map[currentCharacter - 'a']) {
+            rmd(a, idx + 1, newString);
+        } else {
+            newString += currentCharacter;
+            map[currentCharacter - 'a'] = true;
+            rmd(a, idx + 1, newString);
+        }
+    }
+
+    public static void main(String[] args) {
+        String a = "abbabccdacb";
+        rmd(a, 0, "");
+    }
+}
