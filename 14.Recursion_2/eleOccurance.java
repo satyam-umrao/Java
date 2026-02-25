27b9bdf789f061f485091c5c8c100a078db26142
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-25 20:56:24 +0530

    code added

--- 14.Recursion_2/eleOccurance.java
+++ 14.Recursion_2/eleOccurance.java
@@ -1,3 +1,27 @@
 public class eleOccurance {
+    public static int first = -1;
+    public static int last = -1;
 
+    public static void occurance(String a, int idx, char elem) {
+        if (idx == a.length()) {
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char current = a.charAt(idx);
+        if (current == elem) {
+            if (first == -1) {
+                first = idx;
+            } else {
+                last = idx;
+            }
+        }
+        occurance(a, idx + 1, elem);
+    }
+
+    public static void main(String[] args) {
+        String a = "jhasdfgashdf";
+        char elem = 's';
+        occurance(a, 0, elem);
+    }
 }
