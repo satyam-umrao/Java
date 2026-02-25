6caf257ee5a710c691b4eb75e1fcc2faa6994c51
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-25 21:14:11 +0530

    code added

---
+++ 14.Recursion_2/incrArray.java
@@ -0,0 +1,18 @@
+public class incrArray {
+    public static boolean sorted(int arr[], int idx) {
+        if (idx == arr.length - 1) {
+            return true;
+        }
+        if (arr[idx] < arr[idx + 1]) {
+            return sorted(arr, idx + 1);
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        boolean result = sorted(arr, 0);
+        System.out.println(result);
+    }
+}
