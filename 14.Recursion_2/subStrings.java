ecf4d4f1149428c3d4f9225341a6cf2335db5894
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-25 23:03:21 +0530

    code added

---
+++ 14.Recursion_2/subStrings.java
@@ -0,0 +1,20 @@
+// find all the substrings of a string
+public class subStrings {
+    public static void subst(String a, int idx, String newString) {
+        if (idx == a.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char current_character = a.charAt(idx);
+        // choose
+        subst(a, idx + 1, newString + current_character);
+
+        // not choose
+        subst(a, idx + 1, newString);
+    }
+
+    public static void main(String[] args) {
+        String a = "abc";
+        subst(a, 0, "");
+    }
+}
