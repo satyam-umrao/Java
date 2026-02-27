ac2334c036dba4d069b3dc026239f45c67681ced
Satyam Umrao <satyamumrao11225@gmail.com>
2026-02-27 23:03:07 +0530

    code added

---
+++ 15.Recursion_3/paths.java
@@ -0,0 +1,25 @@
+// calculate the no of paths from (0,0) to (n-1,n-1) in a grid of size n*n
+public class paths {
+    public static int pathCalc(int i, int j, int n, int m) {
+        if (i == n || j == m) {
+            return 0;
+        }
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        }
+        // move only downwards
+        int downpath = pathCalc(i + 1, j, n, m);
+        // move only upwards
+        int rightPath = pathCalc(i, j + 1, n, m);
+        // total paths
+        int totalPaths = downpath + rightPath;
+        return totalPaths;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 3;
+        int path = pathCalc(0, 0, n, m);
+        System.out.println(path);
+    }
+}
